d5246ad6702090fc8a99a4a09641c6584e39960f
Sergey Nikitin <snikitin.de@gmail.com>
2021-02-23 21:59:02 +0300

    Improvement of the documentation of FormControls class

--- Wheel Tension Application/FormControls.cs
+++ Wheel Tension Application/FormControls.cs
@@ -47,14 +47,18 @@ namespace Wheel_Tension_Application
             int offsetX,
             int offsetY)
         {
+            // Количество элементов управления формы в GroupBox.
             var itemsCount = groupBox.Controls.OfType<Control>().Count();
 
+            // Отступ от элемента управления формы по Y.
             int indent = offsetY;
 
             if (controlCount < itemsCount)
             {
+                // Поиск элементов управления формы по указанному имени.
                 foreach (Control item in groupBox.Controls.OfType<Control>().Reverse())
                 {
+                    // Если элемент с указанными именем найден, то удаляем его из GroupBox.
                     if (item.Name.IndexOf(controlName) > -1)
                     {
                         groupBox.Controls.Remove(item);
@@ -64,24 +68,35 @@ namespace Wheel_Tension_Application
 
             for (int i = 0; i < controlCount; i++)
             {
+                // Индекс добавляемого элемента.
                 var indexAdd = i + 1;
 
+                // Тип элемента для добавления.
                 Type type = controlForAdding.GetType();
+                // Конструктор элемента для добавления.
                 ConstructorInfo ctor = type.GetConstructor(Type.EmptyTypes);
+                // Элемент управления формы для добавления.
                 Control newControl = (Control)ctor.Invoke(null);
 
+                // Имя добавляемого элемента управления формы.
                 newControl.Name = controlName + indexAdd;
+                // Местоположение добавляемого элемента управления формы.
                 newControl.Location = new Point(offsetX, indent);
 
                 // Установка свойств добавляемого элемента управления формы из указанного элемента с заданными свойствами
                 foreach (string property in controlProperties)
                 {
+                    // Получение свойств элемента управления формы из элемента для добавления.
                     PropertyInfo propertyInfo = controlForAdding.GetType().GetProperty(property);
+
+                    // Установка свойств из элемента для добавления в добавляемый элемент управления формы.
                     propertyInfo.SetValue(newControl, Convert.ChangeType(propertyInfo.GetValue(controlForAdding), propertyInfo.PropertyType), null);
                 }
 
+                // Добавление элемента управления формы в GroupBox.
                 groupBox.Controls.Add(newControl);
 
+                // Определение отступа для следующего добавляемого элемента управления формы.
                 indent += controlHeight + stepBetweenControls;
             }
         }
@@ -109,6 +124,7 @@ namespace Wheel_Tension_Application
             int offsetX,
             int offsetY)
         {
+            // NumericUpDown, который будет добавлен на форму.
             NumericUpDown numericUpDown = new NumericUpDown()
             {
                 Minimum = 0,
@@ -118,6 +134,7 @@ namespace Wheel_Tension_Application
                 Size = new Size(controlWidth, controlHeight)
             };
 
+            // Добавление группы NumericUpDown в GroupBox.
             AddGroupControlsToGroupBox(
                 groupBox,
                 numericUpDown,
@@ -154,12 +171,14 @@ namespace Wheel_Tension_Application
             int offsetX,
             int offsetY)
         {
+            // TextBox, который будет добавлен на форму.
             TextBox textBox = new TextBox()
             {
                 Enabled = false,
                 Size = new Size(controlWidth, controlHeight)
             };
 
+            // Добавление группы NumericUpDown в GroupBox.
             AddGroupControlsToGroupBox(
                 groupBox,
                 textBox,
@@ -193,10 +212,13 @@ namespace Wheel_Tension_Application
         /// <returns>Словарь значений указанных элементов из GroupBox.</returns>
         public Dictionary<Control, string> GetValuesFromGroupControls(GroupBox groupBox, string controlsName)
         {
+            // Словарь значений элементов управления формы.
             var values = new Dictionary<Control, string>() { };
 
+            // Поиск элементов управления формы по указанному имени.
             foreach (Control item in groupBox.Controls.OfType<Control>())
             {
+                // Если элемент с указанными именем найден, то добавляем его текст в словарь значений элементов управления формы.
                 if (item.Name.IndexOf(controlsName) > -1)
                 {
                     values.Add(item, item.Text);
@@ -210,16 +232,18 @@ namespace Wheel_Tension_Application
         /// <summary>
         /// Установка значений выпадающего списка ComboBox.
         /// </summary>
-        /// <param name="comboBox">ComboBox в который будут добавлены значения.</param>
-        /// <param name="values">Значения, которые будут добавлены в выпадающий список ComboBox.</param>
-        /// <param name="isAddRange">Добавлять ли значения в выпадающий список ComboBox.</param>
-        /// <param name="isEnabled">Флаг доступности ComboBox.</param>
+        /// <param name="groupBox">GroupBox, в группу элементов управления которого будут добавлены значения из массива значений для добавления.</param>
+        /// /// <param name="controlsName">Имя элементов управления формы значения которых надо получить.</param>
+        /// <param name="valuesForAdding">Массив значений, которые будут добавлены в группу элементов управления формы.</param>
         public void SetValuesToGroupControlsText(GroupBox groupBox, string controlsName, string[] valuesForAdding)
         {
+            // Индекс элемента управления формы в GroupBox.
             int index = 0;
 
+            // Поиск элементов управления формы по указанному имени.
             foreach (Control item in groupBox.Controls.OfType<Control>())
             {
+                // Если элемент с указанными именем найден, то меняем его текст на значение из массива значения для добавления.
                 if (item.Name.IndexOf(controlsName) > -1)
                 {
                     item.Text = valuesForAdding[index];
@@ -239,13 +263,17 @@ namespace Wheel_Tension_Application
         /// <param name="isEnabled">Флаг доступности ComboBox.</param>
         public void SetComboBoxValue(ComboBox comboBox, List<string> values, bool isAddRange, bool isEnabled)
         {
+            // Очистка выпадающего списка ComboBox.
             comboBox.Items.Clear();
 
+            // Если флаг добавления значений в выпадающий список ComboBox истина, то заполняем выпадающий список ComboBox.
             if (isAddRange)
             {
+                // Заполнение выпадающего списка ComboBox.
                 comboBox.Items.AddRange(values.ToArray());
             }
 
+            // Смена состояния доступности ComboBox.
             comboBox.Enabled = isEnabled;
         }
 
@@ -257,17 +285,22 @@ namespace Wheel_Tension_Application
         /// <returns>Массив значений строк из DataGridView.</returns>
         public List<string[]> GetDataGridViewValues(DataGridView dataGridView)
         {
+            // Массив значений строк.
             var values = new List<string[]>();
 
+            // Заполнение массива значений строк из DataGRidView.
             for (var i = 0; i < dataGridView.Rows.Count; i++)
             {
+                // Значения строк в столбцах.
                 var columnValues = new string[dataGridView.Columns.Count];
 
+                // Чтение значения ячейки и заполнение результирующей строки.
                 for (var j = 0; j < dataGridView.Columns.Count; j++)
                 {
                     columnValues[j] = dataGridView.Rows[i].Cells[j].Value.ToString();
                 }
 
+                // Добавление строки в массив значений строк.
                 values.Add(columnValues);
             }
 
@@ -284,7 +317,9 @@ namespace Wheel_Tension_Application
         /// <param name="rows">Массив значений добавляемых строк.</param>
         public void SetDataGridViewValues(DataGridView dataGridView, int columnCount, string[] rowHeaders, List<string[]> rows)
         {
+            // Установка количества колонок DataGridView.
             dataGridView.ColumnCount = columnCount;
+            // Установка ширины заголовка строк.
             dataGridView.RowHeadersWidth = 165;
 
             try
@@ -294,8 +329,10 @@ namespace Wheel_Tension_Application
                     dataGridView.Columns[i].Name = "";
                 }
 
+                // Очистка строк.
                 dataGridView.Rows.Clear();
 
+                // Если количество строк не совпадает с количеством заголовок строк, то выводим сообщение об ошибке.
                 if (rows.Count != rowHeaders.Length)
                 {
                     MessageBox.Show("Number of row headers must be equal number of rows!", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
@@ -304,7 +341,9 @@ namespace Wheel_Tension_Application
                 {
                     for (var i = 0; i < rows.Count; i++)
                     {
+                        // Добавление строк из массива в DataGRidView.
                         dataGridView.Rows.Add(rows[i]);
+                        // Задание заголовка строки.
                         dataGridView.Rows[i].HeaderCell.Value = rowHeaders[i];
                     }
                 }
